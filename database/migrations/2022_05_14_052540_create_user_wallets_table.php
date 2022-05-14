80a3b91c0b3249cc97666c6d651b82fc464efa37
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-05-14 12:10:44 +0600

    migrations

--- database/migrations/2022_05_14_052540_create_user_wallets_table.php
+++ database/migrations/2022_05_14_052540_create_user_wallets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUserWalletsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_wallets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('role'); //admin, user
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+            $table->mediumText('wallet_number')->nullable();	
+            $table->char('currency', 255)->default("usd");
+            $table->decimal('balance', 20, 2)->default(0.00);	
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_wallets');
     }
 }
---
+++ database/migrations/2022_05_14_054818_create_user_wallet_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserWalletTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_wallet_transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('sender_id');	
+            $table->integer('receiver_id');
+            $table->char('action', 255)->default("");	// transfer, recharge etc 
+            $table->char('sender_currency', 255)->default("");	
+            $table->decimal('amount_in_usd', 20, 2)->default(0.00);	
+            $table->decimal('amount_in_sender_currency', 20, 2)->default(0.00);	
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_wallet_transactions');
+    }
+}
