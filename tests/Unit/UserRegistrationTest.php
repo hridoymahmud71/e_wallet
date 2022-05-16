abbda56c42fc508be542fa511b86133ea08d3432
hridoymahmud71 <hridoymahmud71@gmail.com>
2022-05-16 14:19:39 +0600

    Began writing test cases

---
+++ tests/Unit/UserRegistrationTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class UserRegistrationTest extends TestCase
+{
+
+    public function testSignupWithoutData()
+    {
+
+        $data = [];
+        $header = [
+            'Accept' => '*/*',
+            'Content-Type' => 'application/json'
+        ];
+        //$header = [];
+        $this->json('POST', '/api/auth/user/registration', $data, $header)
+            ->assertStatus(404);
+        // ->assertJsonFragment([
+        //     'result'   => false,
+        // ]);
+    }
+
+    public function testSignupWithSmallPassword()
+    {
+
+        $data = [
+            "name" => "Customer TwoHundred",
+            "email" => "customer200@example.com",
+            "password" => "123",
+            "password_confirmation" => "123"
+        ];
+        $header = [
+            'Accept' => '*/*',
+            'Content-Type' => 'application/json'
+        ];
+        
+        $this->json('POST', '/api/auth/user/registration', $data, $header)
+            ->assertStatus(404);
+        // ->assertJsonFragment([
+        //     'result'   => false,
+        // ]);
+    }
+
+    public function testSignupWithDuplicateEmail()
+    {
+
+        $data = [
+            "name" => "Customer One",
+            "email" => "customer1@example.com",
+            "password" => "123456",
+            "password_confirmation" => "123456"
+        ];
+        $header = [
+            'Accept' => '*/*',
+            'Content-Type' => 'application/json'
+        ];
+        
+        $this->json('POST', '/api/auth/user/registration', $data, $header)
+            ->assertStatus(404);
+        // ->assertJsonFragment([
+        //     'result'   => false,
+        // ]);
+    }
+
+    public function testSignupWithPerfectData()
+    {
+
+        $data = [
+            "name" => "Customer Hundred",
+            "email" => "customer100@example.com",
+            "password" => "123456",
+            "password_confirmation" => "123456"
+        ];
+        $header = [
+            'Accept' => '*/*',
+            'Content-Type' => 'application/json'
+        ];
+
+        $this->json('POST', '/api/auth/user/registration', $data, $header)
+            ->assertStatus(200);
+        // ->assertJsonFragment([
+        //     'result'   => false,
+        // ]);
+    }
+}
